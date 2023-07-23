10a569f565d97da293e8aa773a48260ab608384d
Christian Bondzie <christian.zap@hotmail.fr>
2023-07-23 22:22:36 +0200

    Modification du fonctionnement de l'authentification, retrait du provider. centralisation des méthodes sur la login_card.

--- lib/widgets/registration/login_card.dart
+++ lib/widgets/registration/login_card.dart
@@ -1,24 +1,24 @@
 import 'dart:math';
-
-import 'package:faro/providers/logged_status.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart' as path;
 import '../registration/text_registration_field.dart';
 
-class LoginCard extends ConsumerStatefulWidget {
+class LoginCard extends StatefulWidget {
   const LoginCard({super.key, required this.onLogStatusChange});
 
   final void Function() onLogStatusChange;
 
   @override
-  ConsumerState<LoginCard> createState() {
+  State<LoginCard> createState() {
     return _LoginCardState();
   }
 }
 
-class _LoginCardState extends ConsumerState<LoginCard> {
+class _LoginCardState extends State<LoginCard> {
   final _formKey = GlobalKey<FormState>();
   bool? _isChecked;
   bool _isLogin = true;
@@ -27,23 +27,7 @@ class _LoginCardState extends ConsumerState<LoginCard> {
   String? token;
   String? _enteredEmail;
   String? _enteredPassword;
-
-  @override
-  void initState() {
-    super.initState();
-    _initFormValues();
-  }
-
-  Future<void> _initFormValues() async {
-    final db = await getDatabase();
-    final userLoginData = await db.query('user_login');
-    setState(() {
-      _enteredEmail = userLoginData[0]["email"] as String;
-      _enteredPassword = userLoginData[0]["password"] as String;
-      token = userLoginData[0]["token"] as String;
-      _isChecked = userLoginData[0]["checked"] == 'true';
-    });
-  }
+  bool _initInfoLoaded = false;
 
   void _snackBarMessage(data) {
     ScaffoldMessenger.of(context).clearSnackBars();
@@ -66,15 +50,51 @@ class _LoginCardState extends ConsumerState<LoginCard> {
     }
   }
 
-  Future<String?> _loginRequestion(
-      String enteredEmail, String enteredPassword) async {
+  Future<Database> _getDatabase() async {
+    // récupère le path de la DB
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'faro.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_login(email TEXT, password TEXT, token TEXT)',
+        );
+      },
+      onUpgrade: (db, oldVersion, newVersion) {
+        return db.execute('alter TABLE user_login ADD COLUMN checked TEXT');
+      },
+      version: 2,
+    );
+    return db;
+  }
+
+  void _appendUserLoginData(
+      String email, String password, String token, String isChecked) async {
+    final db = await _getDatabase();
+    final userLoginData = await db.query('user_login');
+
+    final Map<String, String> updatedValue = {
+      'email': email,
+      'password': password,
+      'token': token,
+      'checked': isChecked,
+    };
+
+    if (userLoginData.isEmpty) {
+      await db.insert('user_login', updatedValue);
+    } else {
+      await db.update('user_login', updatedValue);
+    }
+  }
+
+  _loginRequest(String enteredEmail, String enteredPassword) async {
+    // on affiche un loading spiner et désactive les boutons le temps de la requête.
     setState(() {
-      // on affiche un loading spiner et désactive les boutons le temps de la requête.
       _isLoading = true;
     });
 
     // je fais la requête de connexion
-    final url = Uri.parse('http://localhost:3001/login');
+    final url = Uri.http('localhost:3001', 'login');
     final response = await http.post(
       url,
       headers: {'content-type': 'application/json'},
@@ -90,26 +110,98 @@ class _LoginCardState extends ConsumerState<LoginCard> {
       _isLoading = false;
     });
 
-    if (!context.mounted) {
-      return null;
-    }
     final bodyData = json.decode(response.body);
-    // affichage du status de la requête
+    _snackBarMessage(bodyData);
+
     if (response.statusCode >= 400) {
-      _snackBarMessage(bodyData);
       return null;
-    } else {
-      _snackBarMessage(bodyData);
-      // widget.onLogStatusChange();
-      // récupère le token.
-      // print(response.headers);
     }
+
     response.headers.removeWhere((key, value) => key != "authorization");
-    return response.headers["authorization"];
+    final token = response.headers["authorization"];
+
+    _appendUserLoginData(
+        _enteredEmail!, _enteredPassword!, token!, _isChecked.toString());
+
+    widget.onLogStatusChange();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initFormValues();
+  }
+
+  Future<void> _initFormValues() async {
+    final db = await _getDatabase();
+    final userLoginData = await db.query('user_login');
+
+    // 1er cas, première connexion et pas de token.
+    if (userLoginData.isEmpty) {
+      setState(() {
+        _initInfoLoaded = true;
+        _isChecked = false;
+        _enteredEmail = "";
+        _enteredPassword = "";
+        token = "";
+      });
+      return;
+    }
+
+    setState(() {
+      _isChecked = userLoginData[0]["checked"] == 'true';
+    });
+
+    if (_isChecked == false) {
+      // on fait la demande de connexion via le token.
+      final url = Uri.http('localhost:3001', 'login');
+      final response = await http.post(
+        url,
+        headers: {'Authorization': userLoginData[0]["token"] as String},
+      );
+
+      // token valide.
+      if (response.statusCode == 200) {
+        widget.onLogStatusChange();
+        return;
+      }
+
+      // Si le token est invalide.
+      setState(() {
+        _enteredEmail = "";
+        _enteredPassword = "";
+        token = "";
+        _initInfoLoaded = true;
+      });
+
+      return;
+    } else {
+      // on fait la demande de connexion via le token.
+      final url = Uri.http('localhost:3001', 'login');
+      final response = await http.post(
+        url,
+        headers: {'Authorization': userLoginData[0]["token"] as String},
+      );
+
+      // token valide.
+      if (response.statusCode == 200) {
+        widget.onLogStatusChange();
+        return;
+      }
+
+      // Si le token est invalide.
+      setState(() {
+        _enteredEmail = userLoginData[0]["email"] as String;
+        _enteredPassword = userLoginData[0]["password"] as String;
+        token = "";
+        _initInfoLoaded = true;
+      });
+    }
+    //
   }
 
   void _createUserAccount() async {
-    // Récupérer les données users
+    // Récupérer les données utilisateurs
 
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
@@ -123,7 +215,7 @@ class _LoginCardState extends ConsumerState<LoginCard> {
     });
 
     // faire la requête POST et attendre la réponse
-    final url = Uri.parse('http://localhost:3001/signup');
+    final url = Uri.http('localhost:3001', 'signup');
     final response = await http.post(
       url,
       headers: {'content-type': 'application/json'},
@@ -141,21 +233,13 @@ class _LoginCardState extends ConsumerState<LoginCard> {
     setState(() {
       _isLoading = false;
     });
-    // Si positive rediriger vers la page home
     final data = json.decode(response.body);
-
-    if (!context.mounted) {
-      return;
-    }
-
+    _snackBarMessage(data);
     if (response.statusCode >= 400) {
-      _snackBarMessage(data);
       return;
-    } else {
-      _snackBarMessage(data);
     }
 
-    _loginRequestion(_enteredEmail!, _enteredPassword!);
+    _loginRequest(_enteredEmail!, _enteredPassword!);
   }
 
   void _userLogin() async {
@@ -165,17 +249,16 @@ class _LoginCardState extends ConsumerState<LoginCard> {
     } else {
       return;
     }
-    final String? token =
-        await _loginRequestion(_enteredEmail!, _enteredPassword!);
 
-    ref.read(loggedStatusProvider.notifier).appendUserLoginData(
-        _enteredEmail!, _enteredPassword!, token ?? '', _isChecked.toString());
+    _loginRequest(_enteredEmail!, _enteredPassword!);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
+    Widget content = const CircularProgressIndicator();
+
+    if (_initInfoLoaded == true) {
+      content = Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Card(
           child: Padding(
@@ -260,7 +343,9 @@ class _LoginCardState extends ConsumerState<LoginCard> {
                 )),
           ),
         ),
-      ),
-    );
+      );
+    }
+
+    return Center(child: content);
   }
 }
